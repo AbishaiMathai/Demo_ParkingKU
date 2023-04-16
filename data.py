16ea00e7b4e1222f83e570babbd61e0f2382b7b9
Achinth04 <114637549+Achinth04@users.noreply.github.com>
2023-04-16 00:52:39 -0500

    Update data.py
    abishais headache

--- data.py
+++ data.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template, request
-
+import os
+from sqlalchemy import create_engine
+from sqlalchemy import text
+import pandas as pd
+import mysql.connector
 app = Flask(__name__)
+database_string="mysql+pymysql://3vifuwm0dbyvfyrui2fo:pscale_pw_z4giqkBmkL5fzIOPzgePGdENDKMytUhPCOSplsvXeog@aws.connect.psdb.cloud/kuparking?charset=utf8mb4"
+engine=create_engine(database_string,connect_args={
+    "ssl":{
+        "ssl_ca":"/etc/ssl/cert.pem"
+    }
+})
+def()
+with engine.connect() as conn:
+    result = conn.execute(text("select count(ID)  from Templin_1 where Availability=1"))
+    answer=str(result.all())
+    listy=[]
+    for i in answer:
+        if i.isnumeric():
+            listy.append(i)
+    number=int(''.join(listy))
+    print(number)
 
 @app.route('/')
 def index():
@@ -12,9 +32,18 @@ def search():
     print(query)
 
     if query=='Templin Hall':
-       return render_template('templin.html', query=query)
-    else:
+        with engine.connect() as conn:
+            result = conn.execute(text("select count(ID)  from Templin_1 where Availability=0"))
+            answer = str(result.all())
+            listy = []
+            for i in answer:
+                if i.isnumeric():
+                    listy.append(i)
+            number = int(''.join(listy))
+        return render_template('templin.html', query=number)
+    elif query=="ellsworth":
         return render_template('king.html',query=query)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
@@ -22,16 +51,4 @@ if __name__ == '__main__':
 
 
 
-'''from flask import Flask,render_template,request,redirect,url_for
-app=Flask("__name__")
-@app.route("/",methods=['POST','GET'])
-def home():
-    if request.method=='POST':
-        area=request.form['search_input']
-        return render_template('index.html',area=area)
-    else:
-        area = request.form['search_input']
-        return render_template('index.html', area=area)'''
 
-#if __name__=="__main__":
-   # app.run(debug=True)
